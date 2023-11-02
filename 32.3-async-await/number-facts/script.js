4f51bb40c49568c149972ccee3a831a44588cde3
povermanr <rachel.poverman@gmail.com>
2023-11-01 20:22:42 -0400

    32.3 part 1 complete

--- 32.3-async-await/number-facts/script.js
+++ 32.3-async-await/number-facts/script.js
@@ -1,18 +1,46 @@
-function getNumberFact(number) {
-    return axios.get(`http://numbersapi.com/${number}?json`)
-        .then((response) => response.data.text);
+const baseURL = 'http://numbersapi.com';
+
+// async function getNumberFact(number) {
+//     try {
+//         const response = await axios.get(`${baseURL}/${number}?json`);
+//         return response.data.text;
+//     } catch (error) {
+//         console.log('Error:', error);
+//     }
+// }
+
+async function getMultipleFacts(numbers, count) {
+    try {
+        const responses = await Promise.all(
+            numbers.map(number =>
+                axios.get(`${baseURL}/${number}/?json&amount=${count}`)
+            )
+        );
+
+        return responses.map(response => response.data);
+    } catch (error) {
+        console.log('Error:', error);
+    }
 }
 
-const factPromises = [];
+async function displayFacts() {
+    const favNumbers = [7, 13, 44]; 
+    const numFacts = 4;
 
-for (let i = 0; i < 4; i++) {
-    factPromises.push(getNumberFact(13));
+    const facts = await getMultipleFacts(favNumbers, numFacts);
+    console.log('Facts:', facts);
+    const factsContainer = document.getElementById('number-facts');
+
+    for (const numberFacts of facts) {
+        if (Array.isArray(numberFacts)) {
+            const factTexts = numberFacts.map(fact => fact.text);
+            const number = numberFacts[0].number;
+            const numFactsText = factTexts.map((factText, index) => `Fact ${index + 1}: ${factText}`).join('\n');
+            const factElement = document.createElement("div");
+            factElement.textContent = `Facts for Number ${number}:\n${numFactsText}`;
+            factsContainer.appendChild(factElement);
+        }
+    }
 }
 
-Promise.all(factPromises)
-    .then(facts => {
-        facts.forEach(fact => {
-            console.log(fact);
-        });
-    })
-    .catch(err => console.error(err));
+displayFacts();
